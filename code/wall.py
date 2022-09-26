005a6a00761b4d60f17e41d33306e169100cfd44
Michael Miele-Herndon <mikemh@uri.edu>
2022-09-26 15:06:39 -0400

    Update wall.py

--- code/wall.py
+++ code/wall.py
@@ -9,6 +9,12 @@ class Wall(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
     def change_pos(self, add_x, add_y):
+        # moveable walls?
         self.xpos += add_x
         self.ypos += add_y
+
+    def update(self, camera_ref):
+        # take into account camera scroll when setting position
+        self.rect.x = self.xpos + camera_ref.x_scroll
+        self.rect.y = self.ypos + camera_ref.y_scroll
 
