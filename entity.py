d5aed77a47c685b58d96f96c9a2ed0df736bdb83
Michael Miele-Herndon <mikemh@uri.edu>
2022-10-04 09:36:35 -0400

    Update entity.py

--- entity.py
+++ entity.py
@@ -2,23 +2,19 @@ import pygame
 
 
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, x, y, img):
+    def __init__(self, x, y, img, world):
         super().__init__()
         self.xpos, self.ypos = x, y
         self.image = pygame.image.load(img)
         self.rect = self.image.get_rect()
+        self.speed = 2
+        self.room = world
         self.rect.x, self.rect.y = self.xpos, self.ypos
-        self.direction = 1
 
-    def move(self, speed):
-        if self.direction == 1:
-            self.change_x(speed)
-        elif self.direction == -1:
-            self.change_x(-speed)
-        elif self.direction == 2:
-            self.change_y(speed)
-        elif self.direction == -2:
-            self.change_y(-speed)
+    def move(self, x, y):
+        movex, movey = self.check_for_collisions(x, y)
+        self.change_y(movey)
+        self.change_x(movex)
 
     def change_x(self, add_x):
         self.xpos += add_x
@@ -26,7 +22,18 @@ class Entity(pygame.sprite.Sprite):
     def change_y(self, add_y):
         self.ypos += add_y
 
-    def check_for_collisions(self, walls):
+    def update(self, camera_ref):
+        # take into account camera scroll when setting position
+        self.rect.x = self.xpos + camera_ref.x_scroll
+        self.rect.y = self.ypos + camera_ref.y_scroll
+
+    def check_for_collisions(self, try_x, try_y):
+        walls = self.room.room_sprites
+        move_x, move_y = try_x, try_y
         for wall in walls:
-            #  idk how
-            pass
+            if wall.collidepoint == self.collidepoint:
+                move_y = -try_y
+            if wall.collidepoint == try_y + self.ypos:
+                move_x = -try_x
+
+        return move_x, move_y
