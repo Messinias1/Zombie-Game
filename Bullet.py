32e3511108df8d60b61b16641df0fb186aa74834
EricsenSemedo <99306164+EricsenSemedo@users.noreply.github.com>
2022-10-07 14:57:30 -0400

    Made the variables private

--- Bullet.py
+++ Bullet.py
@@ -3,22 +3,22 @@ import math
 
 class Bullet:
     def __init__(self, x:int, y:int , mouse_x, mouse_y) -> None:
-        self.x = x
-        self.y = y
-        self.mouse_x = mouse_x
-        self.mouse_y = mouse_y
-        self.angle = math.atan2(y - mouse_y, x - mouse_x)
-        self.speed = 15
-        self.x_velocity = math.cos(self.angle) * self.speed
-        self.y_velocity = math.sin(self.angle) * self.speed
+        self.x_ = x
+        self.y_ = y
+        self.mouse_x_ = mouse_x
+        self.mouse_y_ = mouse_y
+        self.angle_ = math.atan2(y - mouse_y, x - mouse_x)
+        self.speed_ = 15
+        self.x_velocity_ = math.cos(self.angle_) * self.speed_
+        self.y_velocity_ = math.sin(self.angle_) * self.speed_
 
     def get_position(self):
-        position = (self.x, self.y)
+        position = (self.x_, self.y_)
         return position
         
     def main(self, screen):
-        self.x -= int(self.x_velocity)
-        self.y -= int(self.y_velocity)
+        self.x_ -= int(self.x_velocity_)
+        self.y_ -= int(self.y_velocity_)
         black = 0, 0, 0
 
-        pygame.draw.circle(screen, black, (self.x, self.y), 5)
+        pygame.draw.circle(screen, black, (self.x_, self.y_), 5)
