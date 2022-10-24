f322f9b5a5954011acb8378d3aa9528957f916ed
Jake Murphy <jake_murphy@uri.edu>
2022-10-24 15:03:54 -0400

    button class

--- f22m-main/Button/Button.py
+++ f22m-main/Button/Button.py
@@ -1,18 +1,8 @@
-import string, sys
+import string
 import pygame as pg
-
-# helper website https://www.thepythoncode.com/article/make-a-button-using-pygame-in-python
-
-pg.init()
-fps = 60
-fpsClock = pg.time.Clock()
-width, height = 800, 600
-screen = pg.display.set_mode((width, height))
-objects = []
 
 class Button:
     font = pg.font.SysFont('Arial', 30)
-    
     width:int
     height:int
     position_x:int
@@ -44,49 +34,8 @@ class Button:
             'pressed': (100, 100, 100)
         }
 
-        self.buttonSurface = pg.Surface((self.width, self.height))
-        self.buttonRect = pg.Rect(self.position_x, self.position_y, self.width, self.height)
 
-        self.buttonSurf = self.font.render(self.text, True, (20, 20, 20))
-
-        objects.append(self)
-
-    def process(self):
-        mousePos = pg.mouse.get_pos()
-        self.buttonSurface.fill(self.fillColors['normal'])
-        if self.buttonRect.collidepoint(mousePos):
-            self.buttonSurface.fill(self.fillColors['hover'])
-            if pg.mouse.get_pressed(num_buttons=3)[0]:
-                self.buttonSurface.fill(self.fillColors['pressed'])
-                if self.one_press:
-                    self.on_click_function()
-                elif not self.alreadyPressed:
-                    self.on_click_function()
-                    self.alreadyPressed = True
-            else:
-                self.alreadyPressed = False
-
-        self.buttonSurface.blit(self.buttonSurf, [
-            self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
-            self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
-        ])
-        screen.blit(self.buttonSurface, self.buttonRect)
-
-def myFunction():
-    print('Goodbye :(')
-    pg.quit()
-
-start_button = Button(some_width = 140, some_height = 50, some_position_x = 300, some_position_y = 300, some_text = 'Quit', some_on_click_function = myFunction, some_already_pressed = True)
-
-while True:
-    screen.fill((20, 20, 20))
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
-    for object in objects:
-        object.process()
-    pg.display.flip()
-    fpsClock.tick(fps)
+# How to call button example:
+# start_button = Button(some_width = 140, some_height = 50, some_position_x = 300, some_position_y = 300, some_text = 'Quit', some_on_click_function = myFunction, some_already_pressed = True)
 
     
