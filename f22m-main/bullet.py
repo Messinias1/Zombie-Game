79df5b112f522a35cba281a06ad76144b01e3dd7
EricsenSemedo <99306164+EricsenSemedo@users.noreply.github.com>
2022-10-27 15:16:24 -0400

    Added Comments and renamed a method

--- f22m-main/bullet.py
+++ f22m-main/bullet.py
@@ -1,8 +1,18 @@
 import pygame
 import math
 
+"""This creats a new Bullet on screen
+"""
 class Bullet:
-    def __init__(self, x:int, y:int , mouse_x, mouse_y) -> None:
+    """This creates a new Rational Object
+
+        Parameters:
+            x {int}: takes an integer
+            y {int}: takes an integer
+            mouse_x {int}: takes an integer
+            mouse_y {int}: takes an integer
+    """
+    def __init__(self, x:int, y:int , mouse_x:int, mouse_y:int) -> None:
         self.x_ = x
         self.y_ = y
         self.mouse_x_ = mouse_x
@@ -12,11 +22,20 @@ class Bullet:
         self.x_velocity_ = math.cos(self.angle_) * self.speed_
         self.y_velocity_ = math.sin(self.angle_) * self.speed_
 
+    """This Returns the position of the bullet
+        Return:
+            current position
+    """
     def get_position(self):
         position = (self.x_, self.y_)
         return position
-        
-    def main(self, screen):
+    
+    """This Updates the Bullet position on the screen
+
+        Parameters:
+            screen {Screen}: takes in the screen object 
+    """
+    def updatePosition(self, screen):
         self.x_ -= int(self.x_velocity_)
         self.y_ -= int(self.y_velocity_)
         black = 0, 0, 0
