59c68f367eba2776230d664b33e10a415461ce1e
Michael Miele-Herndon <mikemh@uri.edu>
2022-10-31 14:40:05 -0400

    Update character.py

--- f22m-main/character.py
+++ f22m-main/character.py
@@ -10,7 +10,6 @@ class Character(pygame.sprite.Sprite):
            img --> image for the character sprite
            in_room --> world object where this character is drawn"""
         super().__init__()
-        self.flip = False
         self.images = []
         [self.images.append(imgpath + "/idle/" + filename) for filename in os.listdir(imgpath + "/idle/")]
         # the image path dir will have multiple images, where each one is a frame in the character's animation
@@ -18,14 +17,17 @@ class Character(pygame.sprite.Sprite):
         # I only set the main image to the first file found in the dir
         self.image = pygame.image.load(self.images[0])
         self.rect = self.image.get_rect()
+        self.dir = "left"
         self.world = in_room
         self.xpos, self.ypos = x, y
         self.rect.center = (x, y)
 
-    def flip_char(self, surface):
-        flipped_image = pygame.transform.flip(self.image, True, False)
-        surface.blit(flipped_image, self.rect)
+    def draw(self, surface):
+        pygame.draw.rect(surface, constants.RED, self.rect)
 
+    def flip_char(self):
+        flipped_image = pygame.transform.flip(self.image, True, False)
+        self.image = flipped_image
 
     def move(self, dx, dy):
         move_x, move_y = self.check_for_collisions(dx, dy)
@@ -33,6 +35,12 @@ class Character(pygame.sprite.Sprite):
 
     def change_x_and_y(self, add_x, add_y):
         # control diagonal movement
+        if self.dir == "left" and add_x < 0:
+            self.dir = "right"
+            self.flip_char()
+        if self.dir == "right" and add_x > 0:
+            self.dir = "left"
+            self.flip_char()
         if add_x != 0 and add_y != 0:
             add_x = add_x * (math.sqrt(2) / 2)
             add_y = add_y * (math.sqrt(2) / 2)
@@ -40,13 +48,12 @@ class Character(pygame.sprite.Sprite):
         self.xpos += add_x
         self.ypos += add_y
 
-
     def check_for_collisions(self, try_x=None, try_y=None):
         if try_x is None:
             try_x = 0
         if try_y is None:
             try_y = 0
-        walls = self.world.room_sprites
+        walls = self.world.room_wall_group
         move_x, move_y = try_x, try_y
         for wall in walls:
             if wall.rect.collidepoint(self.rect.x + (wall.width/2), self.rect.y + try_y + (wall.height/2)):  # collisioin going in y direction
