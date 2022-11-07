62174ed7d6728e41c4ee23900f6973722a149ad5
Michael Miele-Herndon <mikemh@uri.edu>
2022-11-07 15:11:59 -0500

    add camera bounds

--- f22m-main/camera.py
+++ f22m-main/camera.py
@@ -16,3 +16,16 @@ class Camera:
     def follow_character(self, char):
         # make the camera follow the given character
         self.x_scroll, self.y_scroll = -char.xpos+(constants.SCREEN_WIDTH/2), -char.ypos+(constants.SCREEN_HEIGHT/2)
+        in_room = char.world
+        # don't let the camera go further to the right
+        if self.x_scroll - constants.SCREEN_WIDTH < -in_room.ROOM_DIMENSIONS[0]:
+            self.x_scroll = -in_room.ROOM_DIMENSIONS[0] + constants.SCREEN_WIDTH
+        # don't let the camera go further to the left
+        if self.x_scroll > 0:
+            self.x_scroll = 0
+        # don't let the camera go further below
+        if self.y_scroll - constants.SCREEN_HEIGHT < -in_room.ROOM_DIMENSIONS[1]:
+            self.y_scroll = -in_room.ROOM_DIMENSIONS[1] + constants.SCREEN_HEIGHT
+        # don't let the camera go further above
+        if self.y_scroll > 0:
+            self.y_scroll = 0
