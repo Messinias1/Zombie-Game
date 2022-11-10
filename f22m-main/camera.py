e44b9801e06369ec01ee74e1d9bd617814811570
Michael Miele-Herndon <mikemh@uri.edu>
2022-11-10 12:44:12 -0500

    Update camera.py

--- f22m-main/camera.py
+++ f22m-main/camera.py
@@ -17,15 +17,15 @@ class Camera:
         # make the camera follow the given character
         self.x_scroll, self.y_scroll = -char.xpos+(constants.SCREEN_WIDTH/2), -char.ypos+(constants.SCREEN_HEIGHT/2)
         in_room = char.world
-        # don't let the camera go further to the right
+        # don't let the camera go to the right of the edge of the map
         if self.x_scroll - constants.SCREEN_WIDTH < -in_room.ROOM_DIMENSIONS[0]:
             self.x_scroll = constants.SCREEN_WIDTH - in_room.ROOM_DIMENSIONS[0]
-        # don't let the camera go further to the left
+        # don't let the camera go to the left of the edge of the map
         if self.x_scroll > 0:
             self.x_scroll = 0
-        # don't let the camera go further below
+        # don't let the camera go below the edge of the map
         if self.y_scroll - constants.SCREEN_HEIGHT < -in_room.ROOM_DIMENSIONS[1]:
             self.y_scroll = constants.SCREEN_HEIGHT - in_room.ROOM_DIMENSIONS[1]
-        # don't let the camera go further above
+        # don't let the camera go above the edge of the map
         if self.y_scroll > 0:
             self.y_scroll = 0
