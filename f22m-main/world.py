52a37eba4ef326a5f816c9aa47c10fe37d1f9f0e
Michael Miele-Herndon <mikemh@uri.edu>
2022-11-14 15:35:58 -0500

    add pathfinding_maze
    when the room is loaded, the wall rows are added to 'pathfinding_maze', which will be used later for finding paths through walls

--- f22m-main/world.py
+++ f22m-main/world.py
@@ -6,7 +6,8 @@ import camera
 
 class World:
     def __init__(self, layout_file: str):
-        """Class for rooms. When creating a new room for a level, create a new World object.
+        """Class for rooms. When creating a new room for a level, create a new World object,
+           aka each World object is ONE room
 
         Running only the init will NOT prepare the map, it will only store the file where it will later load it from.
         To load the map run self.init_room()
@@ -21,36 +22,39 @@ class World:
         # this will store each pygame.sprite in the current room (including walls)
         # when everything in a room needs to .update(), this group will be used
         self.room_sprite_group = pygame.sprite.Group()
+        self.pathfinding_maze = []
         self.ROOM_DIMENSIONS = [0, 0]
         self._layout_file = layout_file
 
-    def init_room(self, enable_pathfinding: bool = True) -> None:
-        """Set up the room to be drawn by the game
-        :param enable_pathfinding specifies whether to load the layout file into Pathfinding"""
+    def init_room(self) -> None:
+        """Set up the room to be drawn by the game"""
         self.load_room()
 
     def load_room(self) -> None:
-        """Prepares the map and stores it in self.room_sprites & self.room_sprite_group"""
+        """Prepares the map and stores it in self.room_sprites & self.room_sprite_group
+           & initiates pathfinding for the room as well"""
         with open(self._layout_file, "r") as f:
             layout = json.loads(f.read())
         y = 0
         for row in layout:
             x = 0
+            this_row = []
             for char in row:
+                if char == "-":
+                    this_tile = wall.Wall(x, y, wall_type=None, in_room=self)  # img=None signifies this wall is a floor tile
                 if char.lower() == "w":
-                    this_tile = wall.Wall(x, y, "assets/images/walls/wood.jpg", self)
-                    self.room_wall_group.add(this_tile)
-                    self.room_sprite_group.add(this_tile)
+                    this_tile = wall.Wall(x, y, "wood", self)
                 if char.lower() == "b":
-                    this_tile = wall.Wall(x, y, "assets/images/walls/black.gif", self)
-                    self.room_wall_group.add(this_tile)
-                    self.room_sprite_group.add(this_tile)
+                    this_tile = wall.Wall(x, y, "black", self)
                 if char.lower() == "s":
-                    this_tile = wall.Wall(x, y, "assets/images/walls/stone.gif", self)
+                    this_tile = wall.Wall(x, y, "stone", self)
+                if this_tile.wall_type is not None:
                     self.room_wall_group.add(this_tile)
                     self.room_sprite_group.add(this_tile)
+                this_row.append(this_tile)
                 x += 32  # each wall sprite is 32x32 pixels
             y += 32
+            self.pathfinding_maze.append(this_row)
         self.ROOM_DIMENSIONS = [x, y]
 
     def update_room_sprites(self) -> None:
