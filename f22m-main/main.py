9960462e66559e1c086b0946118b4890e30d660b
marcos-remy <113474912+marcos-remy@users.noreply.github.com>
2022-11-14 14:59:38 -0500

    Made item work off what state they're defined as

--- f22m-main/main.py
+++ f22m-main/main.py
@@ -8,7 +8,6 @@ from zombie import Zombie
 from world import World
 from button import Button
 
-
 def handle_input(player):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -67,65 +66,50 @@ def quit_game():
     exit()
 
 # Create Items:
-coin_list = [ Item(400, 200, "assets/images/items/coin_f0.png", 100, "DEFAULT", world_room), 
-              Item(300, 200, "assets/images/items/coin_f0.png", 100, "DEFAULT", world_room)
-]
-
-potion_list = [ Item(200, 200, "assets/images/items/potion_red.png", 100, "Healable", world_room)
+item_list = [ Item(400, 200, "assets/images/items/coin_f0.png", 1, "Coin", world_room, player), 
+              Item(300, 200, "assets/images/items/coin_f0.png", 1, "Coin", world_room, player),
+              Item(200, 200, "assets/images/items/potion_red.png", 100, "Healable", world_room, player)
 
 ]
 
 #Create Sprite Groups:
-coin_sprites = pygame.sprite.Group()
-healable_sprites = pygame.sprite.Group()
+item_sprites = pygame.sprite.Group()
 
 #Add sprites to respective sprite groups here so that they can be drawn:
 world_room.room_sprite_group.add(player)
 world_room.room_sprite_group.add(zombie)
 
-for coin in coin_list:
-    world_room.room_sprite_group.add(coin) 
-    coin_sprites.add(coin) 
-
-for potion in potion_list:
-    world_room.room_sprite_group.add(potion)
-    healable_sprites.add(potion)
+for item in item_list:
+    world_room.room_sprite_group.add(item)
+    item_sprites.add(item)
 
 # main game loop
 run = True
 
 #Coin Text:
-font = pygame.font.SysFont('inkfree', 30, italic=False,bold=True)
-txt = font.render('Coins: ' + str(player.coins), True, (255, 255, 255))
-txt_rect = txt.get_rect()
-
-txt_rect.center = (800-(txt.get_rect().width), 0+(txt.get_rect().height))
+coin_font = pygame.font.SysFont('inkfree', 30, italic=False,bold=True)
+coin_txt = coin_font.render('Coins: ' + str(player.coins), True, (255, 255, 255))
+coin_txt_rect = coin_txt.get_rect()
+coin_txt_rect.center = (800-(coin_txt.get_rect().width), 0+(coin_txt.get_rect().height))
 
 #Health Text:
 health_font = pygame.font.SysFont('inkfree', 30, italic=False,bold=True)
 health_txt = health_font.render('Health: ' + str(player.health), True, (255, 255, 255))
 health_txt_rect = health_txt.get_rect()
-
-health_txt_rect.center = (0+(txt.get_rect().width), 0+(txt.get_rect().height))
+health_txt_rect.center = (0+(health_txt.get_rect().width), 0+(health_txt.get_rect().height))
 
 while run:
     # Background Color
     clock.tick(constants.FPS)
     screen.fill(constants.SURFACE_COLOR)
 
-    #Coin Collision Handeling:
-    if pygame.sprite.spritecollide(player, coin_sprites, True):
-        player.coins += 1
-        txt = font.render('Coins: ' + str(player.coins), True, (255, 255, 255))
+    #Item Collision Handeling:
+    hit_list = pygame.sprite.spritecollide(player, item_sprites, True)
 
-    if pygame.sprite.spritecollide(player, healable_sprites, True):
-        #Heal Player
-        player.health += 1
-        health_txt = health_font.render('Health: ' + str(player.health), True, (255, 255, 255))
+    for item in hit_list:
+        item.picked_up = True
 
-    # draw everything in the room on screen
-    coin_sprites.draw(screen)
-    healable_sprites.draw(screen)
+    item_sprites.draw(screen)
     world_room.room_sprite_group.draw(screen)
 
     # create quit button
@@ -137,16 +121,16 @@ while run:
     # run the .update() functions for everything in the room
     world_room.update_room_sprites()
 
-    for coin in coin_list:
-        coin.update()
-    
-    for potion in potion_list:
-        potion.update()
+    for item in item_list:
+        item.update()
 
     world_room.camera.follow_character(player)
 
-    #Currency Text Display
-    screen.blit(txt, txt_rect)
+    #Text Display
+    coin_txt = coin_font.render('Coins: ' + str(player.coins), True, (255, 255, 255))
+    health_txt = health_font.render('Health: ' + str(player.health), True, (255, 255, 255))
+
+    screen.blit(coin_txt, coin_txt_rect)
     screen.blit(health_txt, health_txt_rect)
 
     pygame.display.update()
